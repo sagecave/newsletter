b456c254dc3c821879a7f943502107a4790f6237
”sagecave” <Andreassage@gmail.com>
2024-11-29 08:57:43 +0100

    lol

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,52 +1,51 @@
-// import { getSub } from "@/app/lib/supabse";
-// import { postSub } from "@/app/lib/supabse";
-// import { revalidatePath } from "next/cache";
-// import { delSub } from "@/app/lib/supabse";
+import { getSub } from "@/app/lib/supabse";
+import { postSub } from "@/app/lib/supabse";
+import { revalidatePath } from "next/cache";
+import { delSub } from "@/app/lib/supabse";
 
-// async function page() {
-//   const subscribers = await getSub();
+async function page() {
+  const subscribers = await getSub();
 
-//   async function postingSub(formData) {
-//     "use server";
-//     const data = {
-//       navn: formData.get("navn"),
-//       email: formData.get("email"),
-//     };
-//     await postSub(data);
+  async function postingSub(formData) {
+    "use server";
+    const data = {
+      navn: formData.get("navn"),
+      email: formData.get("email"),
+    };
+    await postSub(data);
 
-//     revalidatePath("/");
-//   }
+    revalidatePath("/");
+  }
 
-//   return (
-//     <div className="bg-green-500">
-//       <h1 className="text-9xl">All subscribes</h1>
-//       <div className=" flex flex-col justify-center items-center">
-//         <form action={postingSub} className="flex flex-col w-max rounded-lg border-white border-2 gap-3 p-5">
-//           <label htmlFor="navn">Name</label>
-//           <input id="navn" name="navn" type="text" />
-//           <label htmlFor="email">Email</label>
-//           <input id="email" name="email" type="text" />
+  return (
+    <div className="bg-green-500">
+      <h1 className="text-9xl">All subscribes</h1>
+      <div className=" flex flex-col justify-center items-center">
+        <form action={postingSub} className="flex flex-col w-max rounded-lg border-white border-2 gap-3 p-5">
+          <label htmlFor="navn">Name</label>
+          <input id="navn" name="navn" type="text" />
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" type="text" />
+        </form>
+        <ul className="flex flex-row flex-wrap gap-3 mt-4 mb-4">
+          {subscribers.map((sub) => (
+            <li className="bg-green-800 p-3 rounded-lg " key={sub.id}>
+              <a href={`/update/${sub.id}`}>
+                <div>
+                  <p> {sub.navn}</p>
 
-//         </form>
-//         <ul className="flex flex-row flex-wrap gap-3 mt-4 mb-4">
-//           {subscribers.map((sub) => (
-//             <li className="bg-green-800 p-3 rounded-lg " key={sub.id}>
-//               <a href={`/update/${sub.id}`}>
-//                 <div>
-//                   <p> {sub.navn}</p>
+                  <p> {sub.email}</p>
+                </div>
+              </a>
+              {/* <form action={deleteSub(sub.id)}>
+                <button className="bg-red-500 rounded-lg p-5">Delete</button>
+              </form> */}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
 
-//                   <p> {sub.email}</p>
-//                 </div>
-//               </a>
-//               {/* <form action={deleteSub(sub.id)}>
-//                 <button className="bg-red-500 rounded-lg p-5">Delete</button>
-//               </form> */}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default page;
+export default page;
